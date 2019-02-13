3c6230e55920ff3bd840258a785315ca10ffa516
JackAbrams <jackabrams96@gmail.com>
2019-02-12 22:40:56 -0500

    added page number data and nextPage and prevPage methods to vue object

--- src/views/EvaluationViewer.vue
+++ src/views/EvaluationViewer.vue
@@ -3,60 +3,68 @@
     <FormNav :items="[]" style="flex: 0 0 200px; margin-right:24px;"/>
     <div class="form-viewer">
       <form class="">
-        <h1 class="title">Neurodevelopmental Profile</h1>
-        <div class="form-section">
-          <div class="row" style="display:flex">
-            <div class="labelled-input">
-              <label for="date-input">Date</label>
-              <input id="last-edited-input" name="last-edited" type="date"
-              :value="evaluation.lastEdited | dateValue"/>
+        <div class="form-page">
+          <h1 class="title">Neurodevelopmental Profile</h1>
+          <div class="form-section">
+            <div class="row" style="display:flex">
+              <div class="labelled-input">
+                <label for="date-input">Date</label>
+                <input id="last-edited-input" name="last-edited" type="date"
+                :value="evaluation.lastEdited | dateValue"/>
+              </div>
+              <div class="labelled-input">
+                <label for="status-input">Status</label>
+                <input id="status-input" type="text" name="status" :value="evaluation.status"/>
+              </div>
             </div>
-            <div class="labelled-input">
-              <label for="status-input">Status</label>
-              <input id="status-input" type="text" name="status" :value="evaluation.status"/>
-            </div>
-          </div>
-          <div class="row">
-            <div class="labelled-input">
+            <div class="row">
+              <div class="labelled-input">
 
-              <label for="code-input">Code</label>
-              <input id="code-input" type="text" name="code" :value="evaluation.code"/>
+                <label for="code-input">Code</label>
+                <input id="code-input" type="text" name="code" :value="evaluation.code"/>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="form-section">
-          <h2>Student</h2>
-          <div class="row">
+          <div class="form-section">
+            <h2>Student</h2>
+            <div class="row">
+              <div class="labelled-input">
+                <label for="name-input">Name</label>
+                <input id="name-input" type="text" name="name"
+                  :value="student.firstName + ' ' + student.lastName"/>
+              </div>
+            </div>
+            <div class="row">
+              <div class="labelled-input">
+                <label for="sex-input">Sex</label>
+                <select id="sex-input" name="sex">
+                  <option value="m">Male</option>
+                  <option value="f">Female</option>
+                </select>
+              </div>
+              <div class="labelled-input">
+                <label for="age-input">Age</label>
+                <input id="age-input" type="text" name="age" :value="evaluation.age"/>
+              </div>
+            </div>
+          </div>
+          <div class="form-section">
             <div class="labelled-input">
-              <label for="name-input">Name</label>
-              <input id="name-input" type="text" name="name"
-                :value="student.firstName + ' ' + student.lastName"/>
+              <label for="evaluators-input">Evaluator(s)</label>
+              <input id="evaluators-input" type="text" name="age"
+              :value="evaluation.evaluator"/>
             </div>
           </div>
-          <div class="row">
-            <div class="labelled-input">
-              <label for="sex-input">Sex</label>
-              <select id="sex-input" name="sex">
-                <option value="m">Male</option>
-                <option value="f">Female</option>
-              </select>
-            </div>
-            <div class="labelled-input">
-              <label for="age-input">Age</label>
-              <input id="age-input" type="text" name="age" :value="evaluation.age"/>
-            </div>
+          <div class="form-section form-page-nav">
+            <button class="btn btn-info" style="font-size: 1.3em;" @click.prevent="">Next</button>
           </div>
         </div>
-        <div class="form-section">
-          <div class="labelled-input">
-            <label for="evaluators-input">Evaluator(s)</label>
-            <input id="evaluators-input" type="text" name="age"
-            :value="evaluation.evaluator"/>
-          </div>
+
+        <!-- form page 2 -->
+
+        <div class="form-page">
         </div>
-        <div class="form-section form-page-nav">
-          <button class="btn btn-info" style="font-size: 1.3em;" @click.prevent="">Next</button>
-        </div>
+
       </form>
     </div>
   </main>
@@ -66,11 +74,25 @@
 import FormNav from '@/components/FormNav'
 
 export default {
+  data:() => ({
+    page: 1,
+  }),
+  methods: {
+      nextPage() {
+
+      },
+      prevPage() {
+
+      },
+  },
   name:"EvaluationViewer",
   components: {
     FormNav
   },
   computed: {
+    isNew () {
+      return this.$route.params.id === 'new'
+    },
     evaluation () {
       return this.$store.getters.getEvaluationById(this.$route.params.id)
     },
